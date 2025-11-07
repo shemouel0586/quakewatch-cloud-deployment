430dd1c2221efd32671a0dee988ca8582833ddd8
shemouel0586 <148964062+shemouel0586@users.noreply.github.com>
2025-11-07 13:30:29 +0200

    Refactor VPC resources in vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,3 @@
-
-# VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -7,13 +5,11 @@ resource "aws_vpc" "main" {
   tags = { Name = "qw-vpc" }
 }
 
-# Internet access
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = { Name = "qw-igw" }
 }
 
-# Public subnet
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
@@ -22,13 +18,14 @@ resource "aws_subnet" "public_a" {
   tags = { Name = "qw-public-a" }
 }
 
-# Route table + association
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
   tags = { Name = "qw-public-rt" }
 }
 
