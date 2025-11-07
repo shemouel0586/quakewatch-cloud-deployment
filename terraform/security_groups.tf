805eea3666880bf84e91eb16809bff2a80dd8cbb
shemouel0586 <148964062+shemouel0586@users.noreply.github.com>
2025-11-07 14:35:48 +0200

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,15 +1,46 @@
-
 resource "aws_security_group" "ec2" {
   name        = "qw-k3s-sg"
   description = "Allow SSH, HTTP, HTTPS and NodePort range"
   vpc_id      = aws_vpc.main.id
 
-  ingress { description = "SSH";   from_port = 22   to_port = 22   protocol = "tcp" cidr_blocks = [var.allow_ssh_cidr] }
-  ingress { description = "HTTP";  from_port = 80   to_port = 80   protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  ingress { description = "HTTPS"; from_port = 443  to_port = 443  protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  ingress { description = "NodePort"; from_port = 30000 to_port = 32767 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allow_ssh_cidr]
+  }
 
-  egress  { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "NodePort range"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = { Name = "qw-k3s-sg" }
 }
