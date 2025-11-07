114429cc6fcdd73fa8de268771ceed25a0fa2d84
shemouel0586 <148964062+shemouel0586@users.noreply.github.com>
2025-11-07 13:04:00 +0200

    Add outputs for EC2 instance details

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
 
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i ./<your-key>.pem ec2-user@${aws_instance.web.public_ip}"
+}
+
+output "test_url" {
+  value = "http://${aws_instance.web.public_ip}"
+}
