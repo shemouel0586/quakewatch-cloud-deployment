be704920aa24e913c1384c5476df1273dd35c860
shemouel0586 <148964062+shemouel0586@users.noreply.github.com>
2025-11-07 13:48:46 +0200

    Refactor AWS variables with new defaults
    Updated AWS profile variable and CIDR blocks for VPC and public subnet. Added default value for key_name variable.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,21 @@
+variable "aws_profile" {
+  type    = string
+  default = "default"
+}
+
 variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
 
-variable "aws_profile" { type = string  default = "default" }
-
-
 variable "vpc_cidr" {
   type    = string
-  default = "10.10.0.0/16"
+  default = "10.20.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   type    = string
-  default = "10.10.1.0/24"
+  default = "10.20.1.0/24"
 }
 
 variable "instance_type" {
@@ -21,13 +23,12 @@ variable "instance_type" {
   default = "t3.small"
 }
 
-# Set this to an existing EC2 key pair name in your region
 variable "key_name" {
-  type        = string
-  description = "EC2 key pair name"
+  type    = string
+  default = "quakewatch-key"
 }
 
 variable "allow_ssh_cidr" {
   type    = string
-  default = "0.0.0.0/0" # tighten in real use
+  default = "0.0.0.0/0"
 }
